9330c90e97a131cb663fba63124053da745fac9c
Nazila Mathari <89547924+nazmathari1@users.noreply.github.com>
2021-11-19 15:43:55 -0500

    Update testproj1.py

--- testproj1.py
+++ testproj1.py
@@ -1,23 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
-{\colortbl;\red255\green255\blue255;\red251\green0\blue129;\red255\green255\blue255;\red44\green44\blue44;
-\red0\green0\blue0;\red10\green82\blue135;}
-{\*\expandedcolortbl;;\cssrgb\c100000\c7843\c57647;\cssrgb\c100000\c100000\c100000;\cssrgb\c22745\c22745\c22745;
-\cssrgb\c0\c0\c0;\cssrgb\c0\c40000\c60000;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-
-\itap1\trowd \taflags0 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
-\clvertalt\clvertalbase \clshdrawnil \clwWidth15701\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\sl330\partightenfactor0
-
-\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 print\cf5 \strokec5 (\'93Good Morning\'94)\cf4 \strokec4 \
-\'a0\
-\pard\intbl\itap1\pardeftab720\sl330\partightenfactor0
-\cf6 \strokec6 def\cf4 \strokec4  \cf5 \strokec5 main():\cf4 \strokec4 \
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\cf2 \strokec2 print\cf5 \strokec5 (\'93Hello Python\'94)\cf4 \strokec4 \
-\'a0\
-\pard\intbl\itap1\pardeftab720\sl330\partightenfactor0
-\cf2 \strokec2 print\cf5 \strokec5 (\'93Good Evening\'94)\cf4 \strokec4 \cell \lastrow\row
-}
+print(“Good Morning”)
+ 
+def main():
+          print(“Hello Python”)
+ 
+print(“Good Evening”)
